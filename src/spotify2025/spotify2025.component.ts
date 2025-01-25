f1bf59770ab71bdd1b0c72096d7557c12dbc65e1
Ramon Xavier <ramon438xavier@gmail.com>
2025-01-25 16:09:08 -0300

    configurado pagamento spotfy

--- src/spotify2025/spotify2025.component.ts
+++ src/spotify2025/spotify2025.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { BuscarPagamentoDto } from '../commom/dto/buscarPagamentoDto.model';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AtualizarPagamentoParcialDto } from '../commom/dto/atualizarPagamentoParcialDto.model copy';
+import { PagamentoService } from '../commom/service/pagamento.service';
 
 @Component({
   selector: 'app-spotify2025',
@@ -8,9 +13,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Spotify2025Component implements OnInit {
 
-  constructor() { }
+  public pagamentos: BuscarPagamentoDto[] = [];
+  public acessoLiberado: boolean = false;
+  public buscandoPagamentos: boolean = true;
+  public senhaParaAtualizar: string = '';
+  public senhaDefault = "91377"
 
-  ngOnInit() {
-  }
+  constructor(private _pagamentoService: PagamentoService, private _toastService: ToastrService, private _router: Router) { }
 
+    ngOnInit() {
+      this.buscandoPagamentos = true;
+      this._pagamentoService.buscarTodos().then(retorno =>{
+        this.pagamentos = retorno.filter(x=>x.Servico == 'Spotify').sort((a, b) => Number(a.Ordem) - Number(b.Ordem));
+      });
+    }
+
+    public definirComoPago(buscarPagamentoDto: BuscarPagamentoDto):void {
+      let atualizarPagamentoParcial: AtualizarPagamentoParcialDto = {
+        Id: buscarPagamentoDto['id'],
+        Status: 'Pago'
+      }
+
+      buscarPagamentoDto.Atualizando = true;
+      this._pagamentoService.atualizarParcial(atualizarPagamentoParcial).then(() => {
+        this._pagamentoService.buscarTodos().then(retorno =>{
+          this.pagamentos = retorno.filter(x=>x.Servico == 'Spotify').sort((a, b) => Number(a.Ordem) - Number(b.Ordem));
+          this._toastService.success('Registro atualziado com sucesso', 'Chicken Dinner!!');
+          buscarPagamentoDto.Atualizando = false;
+        });
+      });
+    }
+
+    public verificarSenha(senha: string):void {
+      this.acessoLiberado = senha == this.senhaDefault;
+      if(this.acessoLiberado)
+        this._toastService.success('Tudo certo', 'Chicken Dinner!!');
+      else
+        this._toastService.error('Você não pode fazer isso', 'Noob');
+    }
 }
