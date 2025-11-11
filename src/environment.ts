45ec3e00976a46a324eeacb2444c759f81b6513e
Ramon Xavier <ramon438xavier@gmail.com>
2025-11-10 22:34:03 -0300

    Update base URL in environment configuration

--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   hmr: false,
-  base_url: 'https://apigenerator.dronahq.com/api/0-Q_L2gs/controllerpay25',
+  base_url: 'https://retoolapi.dev/B0ALlL/testeretool',
 };
 //https://docs.google.com/spreadsheets/d/1-kgV54w8fGKbxr3l-vXwWsmF5Ruzc5cJ/edit?gid=2101294079#gid=2101294079
